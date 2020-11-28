1d8bf0e46c8618666be5a832da2a17f33d3834bb
JC <jc>
2020-11-28 11:19:29 -0600

    Adding jwt_optional to control data viewable

--- resources/item.py
+++ resources/item.py
@@ -1,6 +1,6 @@
 ## Import libraries
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import jwt_required, get_jwt_claims
+from flask_jwt_extended import jwt_required, get_jwt_claims, jwt_optional, get_jwt_identity
 from models.item import ItemModel
 
 ## Item Class
@@ -94,10 +94,18 @@ class Item(Resource):
 
 ## ItemList Class
 class ItemList(Resource):
-    @jwt_required
-    def get(self):
+    @jwt_optional
+    def get(self) -> tuple:
         """
         Return all items in the current items list
         :return: Items in the database
         """
-        return {'items': [item.json() for item in ItemModel.find_all()]}
+        user_id = get_jwt_identity() ## Give whatever is saved in access token as identity
+
+        items = [item.json() for item in ItemModel.find_all()]
+
+        if user_id:
+            return {'items': items}, 200
+
+        return {'items': [item['name'] for item in items],
+                'message': 'More data available if you login'}, 200
